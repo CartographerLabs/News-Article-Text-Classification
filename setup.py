b5e3dfd50337851b53b668b93fadf484496f283c
James Stevenson <james.l.stevenson@gmail.com>
2020-06-02 22:04:17 +0100

    Updating newspaper3k lib in setup file.

--- setup.py
+++ setup.py
@@ -20,5 +20,5 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
-    install_requires=['feedparser', 'pandas', 'sklearn','numpy','newspaper','pathlib'],
+    install_requires=['feedparser', 'pandas', 'sklearn','numpy','newspaper3k','pathlib'],
 )
