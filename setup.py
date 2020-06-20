ec3036b7e99f2edd11b02044da8308335c30e41b
James Stevenson <james.l.stevenson@gmail.com>
2020-06-20 12:03:28 +0100

    updated setup file

--- setup.py
+++ setup.py
@@ -1,15 +1,12 @@
 import setuptools
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
 setuptools.setup(
     name="News-Article-Topic-Text-Classification",
     version="0.0.2",
     author="James Stevenson",
     author_email="hi@jamesstevenson.me",
     description="A pre-trained text classification model for identifying the topic of a news article.",
-    long_description=long_description,
+    long_description="A pre-trained text classification model for identifying the topic of a news article.",
     long_description_content_type="text/markdown",
     url="https://github.com/user1342/News-Article-Text-Classification",
     packages=["news_classification"],
